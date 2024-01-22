53e68397b92d7d772e2151075948a32ce41da690
Nikk143 <ns3183468@gmail.com>
2024-01-22 23:11:48 +0530

    added a container and ui improvements to currently playing panel

--- coolapp/lib/main.dart
+++ coolapp/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'api_calls.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'dart:convert';
 
 Future main() async {
   await dotenv.load();
@@ -12,7 +11,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
         body: Recentplays(),
       ),
@@ -27,27 +26,60 @@ class Recentplays extends StatelessWidget {
   Widget build(BuildContext context) {
     final user = LastFM(dotenv.env['USERNAME'], dotenv.env['API_KEY']);
     return MaterialApp(
-      home: FutureBuilder<Map<String, dynamic>?>(
-        future: user.getRecentPlays(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const CircularProgressIndicator();
-          } else if (snapshot.hasError || snapshot.data == null) {
-            return const Text('Failed to fetch data.');
-          } else {
-            final apiData = snapshot.data!;
-            final recentTracks = apiData['recenttracks'];
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 28, 0, 15),
+        body: Column(
+          children: [
+            Align(
+                child: Container(
+              alignment: Alignment.topCenter,
+              height: 100,
+              width: 350,
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 173, 26, 16),
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.3), // Shadow color
+                    spreadRadius: 1, // How far the shadow should spread
+                    blurRadius: 4, // How blurry the shadow should be
+                    offset: Offset(5, 5), // Shadow offset (x, y)
+                  ),
+                ],
+              ),
+              child: FutureBuilder<Map<String, dynamic>?>(
+                future: user.getRecentPlays(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const CircularProgressIndicator();
+                  } else if (snapshot.hasError || snapshot.data == null) {
+                    return const Text('Failed to fetch data.');
+                  } else {
+                    final apiData = snapshot.data!;
+                    final recentTracks = apiData['recenttracks'];
 
-            if (recentTracks['track'][0]['@attr']['nowplaying'] == 'true') {
-              final track = recentTracks['track'][0]['name'];
-              final artist = recentTracks['track'][0]['artist']['name'];
-              return Text(
-                  'Currently Playing: ${track} - ${artist}');
-            }
-
-            return const Text('Nothing being played.');
-          }
-        },
+                    try {
+                      if (recentTracks['track'][0]['@attr']['nowplaying'] ==
+                          'true') {
+                        final track = recentTracks['track'][0]['name'];
+                        final artist =
+                            recentTracks['track'][0]['artist']['#text'];
+                        return Center(
+                          child: Text(
+                            'Currently Playing: \n$track - $artist',
+                            textAlign: TextAlign.center,
+                          ),
+                        );
+                      }
+                    } catch (e) {}
+                    return const Text('Nothing being played.');
+                  }
+                },
+              ),
+            ))
+          ],
+        ),
       ),
     );
   }
