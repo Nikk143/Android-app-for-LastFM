d41fa2d5d9b15d82e10e64336c6c562f8642f970
Nikk143 <ns3183468@gmail.com>
2024-01-24 21:00:14 +0530

    code cleanup (currently playing panel)

--- coolapp/lib/main.dart
+++ coolapp/lib/main.dart
@@ -38,68 +38,143 @@ class Recentplays extends StatelessWidget {
       home: Scaffold(
         // backgroundColor: Color.fromARGB(255, 28, 0, 15),
         backgroundColor: Theme.of(context).colorScheme.background,
-        body: Column(
-          children: [
-            Align(
-              child: Container(
-                alignment: Alignment.topCenter,
-                margin: EdgeInsets.only(top: 30),
-                height: 100,
-                width: 350,
-                padding: const EdgeInsets.symmetric(horizontal: 30),
-                decoration: BoxDecoration(
-                  // const Color.fromARGB(255, 173, 26, 16)
-                  color: Theme.of(context).colorScheme.primary,
-                  borderRadius: BorderRadius.circular(10),
-                  boxShadow: [
-                    BoxShadow(
-                      color:
-                          Theme.of(context).colorScheme.shadow.withOpacity(0.4),
-                      spreadRadius: 2.5,
-                      blurRadius: 8,
-                      offset: const Offset(2, 2),
-                    ),
-                  ],
-                ),
-                child: FutureBuilder<Map<String, dynamic>?>(
-                  future: user.getRecentPlays(),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    } else if (snapshot.hasError || snapshot.data == null) {
-                      return const Text(
-                        'Failed to fetch data.',
-                      );
-                    } else {
-                      final apiData = snapshot.data!;
-                      final recentTracks = apiData['recenttracks'];
+        body: FutureBuilder<Map<String, dynamic>?>(
+          future: user.getRecentPlays(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.hasError || snapshot.data == null) {
+              return const Text(
+                'Failed to fetch data.',
+              );
+            } else {
+              final apiData = snapshot.data!;
+              final recentTracks = apiData['recenttracks'];
 
-                      try {
-                        if (recentTracks['track'][0]['@attr']['nowplaying'] ==
-                            'true') {
-                          final track = recentTracks['track'][0]['name'];
-                          final artist =
-                              recentTracks['track'][0]['artist']['#text'];
-                          return Center(
-                            child: Text(
-                              'Currently Playing: \n$track - $artist',
-                              textAlign: TextAlign.center,
-                              style: Theme.of(context).textTheme.bodyMedium,
-                            ),
-                          );
-                        }
-                      } catch (e) {}
-                      return const Text('Nothing being played.');
-                    }
-                  },
-                ),
-              ),
-            )
-          ],
+              try {
+                if (recentTracks['track'][0]['@attr']['nowplaying'] == 'true') {
+                  final track = recentTracks['track'][0]['name'];
+                  final artist = recentTracks['track'][0]['artist']['#text'];
+                  return Align(
+                    alignment: Alignment.topCenter,
+                    child: Container(
+                      margin: EdgeInsets.only(top: 30),
+                      height: 100,
+                      width: 350,
+                      padding: const EdgeInsets.symmetric(horizontal: 30),
+                      decoration: BoxDecoration(
+                        // const Color.fromARGB(255, 173, 26, 16)
+                        color: Theme.of(context).colorScheme.primary,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Theme.of(context)
+                                .colorScheme
+                                .shadow
+                                .withOpacity(0.4),
+                            spreadRadius: 2.5,
+                            blurRadius: 8,
+                            offset: const Offset(2, 2),
+                          ),
+                        ],
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Currently Playing: \n$track - $artist',
+                          textAlign: TextAlign.center,
+                          style: Theme.of(context).textTheme.bodyMedium,
+                        ),
+                      ),
+                    ),
+                  );
+                }
+              } catch (e) {
+                return Container();
+              }
+              return const Center(
+                child: Text('Error 404'),
+              );
+            }
+          },
         ),
       ),
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+// Column(
+//           children: [
+//             Align(
+//               child: Container(
+//                 alignment: Alignment.topCenter,
+//                 margin: EdgeInsets.only(top: 30),
+//                 height: 100,
+//                 width: 350,
+//                 padding: const EdgeInsets.symmetric(horizontal: 30),
+//                 decoration: BoxDecoration(
+//                   // const Color.fromARGB(255, 173, 26, 16)
+//                   color: Theme.of(context).colorScheme.primary,
+//                   borderRadius: BorderRadius.circular(10),
+//                   boxShadow: [
+//                     BoxShadow(
+//                       color:
+//                           Theme.of(context).colorScheme.shadow.withOpacity(0.4),
+//                       spreadRadius: 2.5,
+//                       blurRadius: 8,
+//                       offset: const Offset(2, 2),
+//                     ),
+//                   ],
+//                 ),
+//                 child: FutureBuilder<Map<String, dynamic>?>(
+//                   future: user.getRecentPlays(),
+//                   builder: (context, snapshot) {
+//                     if (snapshot.connectionState == ConnectionState.waiting) {
+//                       return const Center(
+//                         child: CircularProgressIndicator(),
+//                       );
+//                     } else if (snapshot.hasError || snapshot.data == null) {
+//                       return const Text(
+//                         'Failed to fetch data.',
+//                       );
+//                     } else {
+//                       final apiData = snapshot.data!;
+//                       final recentTracks = apiData['recenttracks'];
+
+//                       try {
+//                         if (recentTracks['track'][0]['@attr']['nowplaying'] ==
+//                             'true') {
+//                           final track = recentTracks['track'][0]['name'];
+//                           final artist =
+//                               recentTracks['track'][0]['artist']['#text'];
+//                           return Center(
+//                             child: Text(
+//                               'Currently Playing: \n$track - $artist',
+//                               textAlign: TextAlign.center,
+//                               style: Theme.of(context).textTheme.bodyMedium,
+//                             ),
+//                           );
+//                         }
+//                       } catch (e) {
+//                         return Container();
+//                       }
+//                       return const Center(
+//                         child: Text('Error 404'),
+//                       );
+//                     }
+//                   },
+//                 ),
+//               ),
+//             )
+//           ],
+//         ),
