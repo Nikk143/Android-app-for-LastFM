5fcd7652128af674d8b8a942b82b6ffd2610309c
Nikk143 <ns3183468@gmail.com>
2024-01-26 19:46:33 +0530

    added an appbar and a new home widget renders the recentplays widget now

--- coolapp/lib/main.dart
+++ coolapp/lib/main.dart
@@ -16,10 +16,33 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: defaultTheme,
-      debugShowCheckedModeBanner: false,
-      home: const Scaffold(
-        body: Recentplays(),
+        theme: defaultTheme,
+        debugShowCheckedModeBanner: false,
+        home: const Scaffold(
+          body: Home(),
+        ));
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Recentplays(),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        leadingWidth: 120,
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 20),
+          child: Image.asset(
+            'assets/lastfmlogo.png', // Replace with your image asset path
+            width: 100, // Adjust width as needed
+            height: 100, // Adjust height as needed
+            color: Colors.white,
+          ),
+        ),
       ),
     );
   }
@@ -31,107 +54,103 @@ class Recentplays extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final user = LastFM(dotenv.env['USERNAME'], dotenv.env['API_KEY']);
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: defaultTheme,
-      home: Scaffold(
-        backgroundColor: Theme.of(context).colorScheme.background,
-        body: FutureBuilder<Map<String, dynamic>?>(
-          future: user.getRecentPlays(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            } else if (snapshot.hasError || snapshot.data == null) {
-              return const Text(
-                'Error fetching current track.',
-              );
-            } else {
-              final apiData = snapshot.data!;
-              final recentTracks = apiData['recenttracks'];
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: FutureBuilder<Map<String, dynamic>?>(
+        future: user.getRecentPlays(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError || snapshot.data == null) {
+            return const Text(
+              'Error fetching current track.',
+            );
+          } else {
+            final apiData = snapshot.data!;
+            final recentTracks = apiData['recenttracks'];
 
-              return Align(
-                alignment: Alignment.topCenter,
-                child: Column(
-                  children: [
-                    (isTrackCurrentlyPlaying(recentTracks))
-                        ? Container(
-                            margin: const EdgeInsets.only(top: 30),
-                            height: 100,
-                            width: MediaQuery.of(context).size.width * 0.9,
-                            padding: const EdgeInsets.symmetric(horizontal: 30),
-                            decoration: BoxDecoration(
-                              color: Theme.of(context).colorScheme.primary,
-                              borderRadius: BorderRadius.circular(10),
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Theme.of(context)
-                                      .colorScheme
-                                      .shadow
-                                      .withOpacity(0.4),
-                                  spreadRadius: 2.5,
-                                  blurRadius: 8,
-                                  offset: const Offset(2, 2),
-                                ),
-                              ],
-                            ),
-                            child: Center(
-                              child: Text(
-                                'Currently Playing: \n${recentTracks['track'][0]['name']} - ${recentTracks['track'][0]['artist']['#text']}',
-                                textAlign: TextAlign.center,
-                                style: Theme.of(context).textTheme.bodyMedium,
+            return Align(
+              alignment: Alignment.topCenter,
+              child: Column(
+                children: [
+                  (isTrackCurrentlyPlaying(recentTracks))
+                      ? Container(
+                          margin: const EdgeInsets.only(top: 30),
+                          height: 100,
+                          width: MediaQuery.of(context).size.width * 0.9,
+                          padding: const EdgeInsets.symmetric(horizontal: 30),
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).colorScheme.primary,
+                            borderRadius: BorderRadius.circular(10),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Theme.of(context)
+                                    .colorScheme
+                                    .shadow
+                                    .withOpacity(0.4),
+                                spreadRadius: 2.5,
+                                blurRadius: 8,
+                                offset: const Offset(2, 2),
                               ),
-                            ),
-                          )
-                        : Container(),
-                    Container(
-                      height: 150,
-                      width: MediaQuery.of(context).size.width * 0.9,
-                      margin: EdgeInsets.only(top: 20),
-                      padding: EdgeInsets.all(20),
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).colorScheme.primary,
-                        borderRadius: BorderRadius.circular(10),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Theme.of(context)
-                                .colorScheme
-                                .shadow
-                                .withOpacity(0.4),
-                            spreadRadius: 2.5,
-                            blurRadius: 8,
-                            offset: const Offset(2, 2),
+                            ],
                           ),
+                          child: Center(
+                            child: Text(
+                              'Currently Playing: \n${recentTracks['track'][0]['name']} - ${recentTracks['track'][0]['artist']['#text']}',
+                              textAlign: TextAlign.center,
+                              style: Theme.of(context).textTheme.bodyMedium,
+                            ),
+                          ),
+                        )
+                      : Container(),
+                  Container(
+                    height: 150,
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    margin: const EdgeInsets.only(top: 20),
+                    padding: const EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.primary,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Theme.of(context)
+                              .colorScheme
+                              .shadow
+                              .withOpacity(0.4),
+                          spreadRadius: 2.5,
+                          blurRadius: 8,
+                          offset: const Offset(2, 2),
+                        ),
+                      ],
+                    ),
+                    child: DefaultTextStyle(
+                      style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Align(
+                            alignment: Alignment.topLeft,
+                            child: Text('Recently played:'),
+                          ),
+                          Text('1. ${recentTracks['track'][1]['name']}'),
+                          Text('2. ${recentTracks['track'][2]['name']}'),
+                          Text('3. ${recentTracks['track'][3]['name']}'),
+                          Text('4. ${recentTracks['track'][4]['name']}'),
+                          Text('5. ${recentTracks['track'][5]['name']}'),
                         ],
                       ),
-                      child: DefaultTextStyle(
-                        style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const Align(
-                              alignment: Alignment.topLeft,
-                              child: Text('Recently played:'),
-                              ),
-                            Text('1. ${recentTracks['track'][1]['name']}'),
-                            Text('2. ${recentTracks['track'][2]['name']}'),
-                            Text('3. ${recentTracks['track'][3]['name']}'),
-                            Text('4. ${recentTracks['track'][4]['name']}'),
-                            Text('5. ${recentTracks['track'][5]['name']}'),
-                          ],
-                        ),
-                      ),
                     ),
-                  ],
-                ),
-              );
-            }
-          },
-        ),
+                  ),
+                ],
+              ),
+            );
+          }
+        },
       ),
     );
   }
