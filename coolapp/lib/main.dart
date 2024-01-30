32342110866e2e9852286893dec7fa3aac989914
Nikk143 <ns3183468@gmail.com>
2024-01-30 20:20:55 +0530

    switched to ListView widgets to display home page content, unfixed bug: OverviewStats' width

--- coolapp/lib/main.dart
+++ coolapp/lib/main.dart
@@ -83,79 +83,29 @@ class Recentplays extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      body: FutureBuilder<Map<String, dynamic>?>(
-        future: user.getRecentPlays(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (snapshot.hasError || snapshot.data == null) {
-            return const Text(
-              'Error fetching current track.',
-            );
-          } else {
-            final apiData = snapshot.data!;
-            final recentTracks = apiData['recenttracks'];
+    return FutureBuilder<Map<String, dynamic>?>(
+      future: user.getRecentPlays(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (snapshot.hasError || snapshot.data == null) {
+          return const Text(
+            'Error fetching current track.',
+          );
+        } else {
+          final apiData = snapshot.data!;
+          final recentTracks = apiData['recenttracks'];
 
-            return Align(
-              alignment: Alignment.topCenter,
-              child: Column(
-                children: [
-                  (isTrackCurrentlyPlaying(recentTracks))
-                      ? Container(
-                          margin: const EdgeInsets.only(top: 20),
-                          width: MediaQuery.of(context).size.width * 0.9,
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 30, vertical: 10),
-                          decoration: BoxDecoration(
-                            color: Theme.of(context).colorScheme.primary,
-                            borderRadius: BorderRadius.circular(10),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Theme.of(context)
-                                    .colorScheme
-                                    .shadow
-                                    .withOpacity(0.2),
-                                spreadRadius: 2.0,
-                                blurRadius: 6,
-                                offset: const Offset(2, 2),
-                              ),
-                            ],
-                          ),
-                          child: Center(
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Image.asset(
-                                  'assets/sound.gif',
-                                  width: 40,
-                                  height: 40,
-                                  fit: BoxFit.contain,
-                                ),
-                                const SizedBox(width: 10),
-                                Expanded(
-                                  child: Text(
-                                    '${recentTracks['track'][0]['name']} - ${recentTracks['track'][0]['artist']['#text']}',
-                                    textAlign: TextAlign.center,
-                                    style:
-                                        Theme.of(context).textTheme.bodyMedium,
-                                    overflow: TextOverflow.ellipsis,
-                                    maxLines: 2,
-                                  ),
-                                )
-                              ],
-                            ),
-                          ),
-                        )
-                      : Container(),
-                  Expanded(
-                    child: Container(
+          return Column(
+            children: [
+              (isTrackCurrentlyPlaying(recentTracks))
+                  ? Container(
                       width: MediaQuery.of(context).size.width * 0.9,
-                      margin: const EdgeInsets.symmetric(vertical: 20),
-                      padding: const EdgeInsets.all(20),
+                      margin: const EdgeInsets.only(top: 20),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 30, vertical: 10),
                       decoration: BoxDecoration(
                         color: Theme.of(context).colorScheme.primary,
                         borderRadius: BorderRadius.circular(10),
@@ -171,65 +121,101 @@ class Recentplays extends StatelessWidget {
                           ),
                         ],
                       ),
-                      child: IntrinsicHeight(
-                        child: DefaultTextStyle(
-                          style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Align(
-                                alignment: Alignment.topLeft,
-                                child: Container(
-                                  margin: const EdgeInsets.only(bottom: 10),
-                                  child: const Text('Recently played:'),
-                                ),
-                              ),
-                              Expanded(
-                                child: ListView.builder(
-                                  itemCount: 5,
-                                  itemBuilder: (context, index) {
-                                    return Padding(
-                                      padding: const EdgeInsets.symmetric(
-                                          vertical: 2.0),
-                                      child: Row(
-                                        children: [
-                                          // Adjust the right padding as needed
-                                          ClipRRect(
-                                            borderRadius:
-                                                BorderRadius.circular(4),
-                                            child: Image.network(
-                                              recentTracks['track'][index]
-                                                      ['image'][0]['#text']
-                                                  .toString(),
-                                            ),
-                                          ),
-                                          Expanded(
-                                            child: Text(
-                                              ' ${recentTracks['track'][index]['name']}',
-                                              overflow: TextOverflow.ellipsis,
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    );
-                                  },
-                                ),
-                              )
-                            ],
-                          ),
+                      child: Center(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              'assets/sound.gif',
+                              width: 40,
+                              height: 40,
+                              fit: BoxFit.contain,
+                            ),
+                            const SizedBox(width: 10),
+                            Text(
+                              '${recentTracks['track'][0]['name']} - ${recentTracks['track'][0]['artist']['#text']}',
+                              textAlign: TextAlign.center,
+                              style: Theme.of(context).textTheme.bodyMedium,
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 2,
+                            )
+                          ],
                         ),
                       ),
+                    )
+                  : Container(),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.9,
+                padding: const EdgeInsets.all(20),
+                margin: const EdgeInsets.only(top: 20),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.primary,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color:
+                          Theme.of(context).colorScheme.shadow.withOpacity(0.2),
+                      spreadRadius: 2.0,
+                      blurRadius: 6,
+                      offset: const Offset(2, 2),
+                    ),
+                  ],
+                ),
+                child: DefaultTextStyle(
+                  style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Align(
+                          alignment: Alignment.topLeft,
+                          child: Container(
+                            margin: const EdgeInsets.only(bottom: 10),
+                            child: const Text('Recently played:'),
+                          ),
+                        ),
+                        ListView.builder(
+                          shrinkWrap: true,
+                          physics: NeverScrollableScrollPhysics(),
+                          itemCount: 5,
+                          itemBuilder: (context, index) {
+                            return Container(
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 2.0),
+                              child: Row(
+                                children: [
+                                  ClipRRect(
+                                    borderRadius: BorderRadius.circular(4),
+                                    child: Image.network(
+                                      recentTracks['track'][index]['image'][0]
+                                              ['#text']
+                                          .toString(),
+                                    ),
+                                  ),
+                                  const SizedBox(width: 8),
+                                  Expanded(
+                                    child: Text(
+                                      recentTracks['track'][index]['name'],
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                        )
+                      ],
                     ),
                   ),
-                ],
-              ),
-            );
-          }
-        },
-      ),
+                ),
+              )
+            ],
+          );
+        }
+      },
     );
   }
 
@@ -242,16 +228,24 @@ class Recentplays extends StatelessWidget {
   }
 }
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
   @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Column(
-        children: [
-          Expanded(child: Recentplays()),
-          Expanded(child: OverviewStats()),
+      body: ListView(
+        children: const [
+          Recentplays(),
+          SizedBox(
+            height: 20,
+          ),
+          OverviewStats(),
         ],
       ),
       appBar: AppBar(
@@ -260,9 +254,9 @@ class HomeView extends StatelessWidget {
         leading: Padding(
           padding: const EdgeInsets.only(left: 20),
           child: Image.asset(
-            'assets/lastfmlogo.png', // Replace with your image asset path
-            width: 100, // Adjust width as needed
-            height: 100, // Adjust height as needed
+            'assets/lastfmlogo.png',
+            width: 100,
+            height: 100,
             color: Colors.white,
           ),
         ),
@@ -302,65 +296,57 @@ class OverviewStats extends StatelessWidget {
     DateTime time =
         DateTime(currentTime.year, currentTime.month, currentTime.day);
     int unixTime = (time.toUtc()).millisecondsSinceEpoch ~/ 1000;
-    return Scaffold(
-      body: FutureBuilder<Map<String, dynamic>?>(
-        future: user.getRecentPlays(unixTime: unixTime),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (snapshot.hasError || snapshot.data == null) {
-            return Text(
-              'Error fetching recent plays: ${snapshot.error}',
-            );
-          } else {
-            final apiData = snapshot.data!;
-            final dailyScrobbles = apiData['recenttracks']['track'].length;
+    return FutureBuilder<Map<String, dynamic>?>(
+      future: user.getRecentPlays(unixTime: unixTime),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (snapshot.hasError || snapshot.data == null) {
+          return Text(
+            'Error fetching recent plays: ${snapshot.error}',
+          );
+        } else {
+          final apiData = snapshot.data!;
+          final dailyScrobbles = apiData['recenttracks']['track'].length;
 
-            return Align(
-              alignment: Alignment.topCenter,
-              child: Container(
-                width: MediaQuery.of(context).size.width * 0.9,
-                padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-                decoration: BoxDecoration(
-                  color: Theme.of(context).colorScheme.primary,
-                  borderRadius: BorderRadius.circular(10),
-                  boxShadow: [
-                    BoxShadow(
-                      color:
-                          Theme.of(context).colorScheme.shadow.withOpacity(0.2),
-                      spreadRadius: 2.0,
-                      blurRadius: 6,
-                      offset: const Offset(2, 2),
+          return Container(
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            width: MediaQuery.of(context).size.width * 0.9,
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.primary,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Theme.of(context).colorScheme.shadow.withOpacity(0.2),
+                  spreadRadius: 2.0,
+                  blurRadius: 6,
+                  offset: const Offset(2, 2),
+                ),
+              ],
+            ),
+            child: DefaultTextStyle(
+              style: Theme.of(context).textTheme.bodyMedium!,
+              overflow: TextOverflow.ellipsis,
+              maxLines: 2,
+              child: Center(
+                child: Column(
+                  children: [
+                    Text('Daily Scrobbles: $dailyScrobbles'),
+                    Text(
+                      'Top Artist: ${user.dailyTopArtist.keys.first} - ${user.dailyTopArtist[user.dailyTopArtist.keys.first]} plays',
+                    ),
+                    Text(
+                      'Top Album: ${user.dailyTopAlbum.keys.first} - ${user.dailyTopAlbum[user.dailyTopAlbum.keys.first]} plays',
                     ),
                   ],
                 ),
-                child: IntrinsicHeight(
-                  child: DefaultTextStyle(
-                    style: Theme.of(context).textTheme.bodyMedium!,
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 2,
-                    child: Center(
-                      child: Column(
-                        children: [
-                          Text('Daily Scrobbles: $dailyScrobbles'),
-                          Text(
-                            'Top Artist: ${user.dailyTopArtist.keys.first} - ${user.dailyTopArtist[user.dailyTopArtist.keys.first]} plays',
-                          ),
-                          Text(
-                            'Top Album: ${user.dailyTopAlbum.keys.first} - ${user.dailyTopAlbum[user.dailyTopAlbum.keys.first]} plays',
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
               ),
-            );
-          }
-        },
-      ),
+            ),
+          );
+        }
+      },
     );
   }
 }
